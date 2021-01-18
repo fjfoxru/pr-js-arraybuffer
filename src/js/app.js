a94fc0699e8244530dd483593067bc2438a7011a
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2021-01-18 13:56:18 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,34 @@
 // Задание 2
-import ArrayBufferConverter from './arrayBufferConverter';
-import getBuffer from './getBuffer';
+// import ArrayBufferConverter from './arrayBufferConverter';
+// import getBuffer from './getBuffer';
 
 
-const testArrayBuffer = getBuffer();
+// const testArrayBuffer = getBuffer();
 
-const arrayBufferConverter = new ArrayBufferConverter();
-arrayBufferConverter.load(testArrayBuffer);
-console.log(arrayBufferConverter.buffer);
-console.log(arrayBufferConverter.toString());
+// const arrayBufferConverter = new ArrayBufferConverter();
+// arrayBufferConverter.load(testArrayBuffer);
+// console.log(arrayBufferConverter.buffer);
+// console.log(arrayBufferConverter.toString());
 
 
 
 
 // Задание 1
-// import Magician from './magician';
-// import Daemon from './daemon';
-// const magicial = new Magician(100);
-// const daemon = new Daemon(100);
-// daemon.hit(1)
-// console.log(daemon.attack)
-// magicial.hit(1)
-// console.log(magicial.attack)
-// magicial.hit(2)
-// console.log(magicial.attack)
-// magicial.stonedHit = true;
-// console.log(magicial.attack)
-// magicial.hit(2)
-// magicial.hit(5)
-// magicial.hit(5)
-// console.log(magicial.attack)
-// console.log(magicial)
+import Magician from './magician';
+import Daemon from './daemon';
+const magicial = new Magician(100);
+const daemon = new Daemon(100);
+
+
+daemon.hit(3)
+console.log(daemon.attack, 'атака daemon по третьей клетке')
+daemon.stoned = true;
+daemon.hit(3)
+console.log(daemon.attack, 'атака daemon под stoned по третьей клетке')
+
+
+magicial.hit(2)
+console.log(magicial.attack)
+magicial.stoned = true;
+magicial.hit(2)
+console.log(magicial.attack)
