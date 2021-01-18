f6ce6fb5aa00a379aa89f571c657cdb2c75b68f0
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2021-01-18 13:58:26 +0300

    Update math.test.js

--- src/js/__tests__/math.test.js
+++ src/js/__tests__/math.test.js
@@ -1,51 +1,50 @@
-import Math from '../math';
+import MathGame from '../mathGame';
 
-const math = new Math();
 
 test('math создан, дефолтная атака = 100', () => {
-  const magicial = new Math(100);
+  const magicial = new MathGame(100);
   expect(magicial.attack).toBe(100);
 });
 
 test('маг под stoned', () => {
-    const magicial = new Math(100);
-    magicial.stonedHit = true;
+    const magicial = new MathGame(100);
+    magicial.stoned = true;
     expect(magicial.stoned).toBe(true);
   });
 
-  test('атака мага под stoned первой клетки меньше на 10', () => {
-    const magicial = new Math(100);
-    magicial.stonedHit = true;
-    magicial.hit(1)
-    expect(magicial.attack).toBe(90);
+  test('атака мага под stoned первой клетки меньше на 15', () => {
+    const magicial = new MathGame(100);
+    magicial.stoned = true;
+    magicial.hit(2)
+    expect(magicial.attack).toBe(85);
   });
   test('атака мага 1 клетки 100%', () => {
-    const magicial = new Math(100);
+    const magicial = new MathGame(100);
     magicial.hit(1)
     expect(magicial.attack).toBe(100);
   });
   test('атака мага 2 клетки 90%', () => {
-    const magicial = new Math(100);
+    const magicial = new MathGame(100);
     magicial.hit(2)
     expect(magicial.attack).toBe(90);
   });
   test('атака мага 3 клетки 80%', () => {
-    const magicial = new Math(100);
+    const magicial = new MathGame(100);
     magicial.hit(3)
     expect(magicial.attack).toBe(80);
   });
   test('атака мага 4 клетки 70%', () => {
-    const magicial = new Math(100);
+    const magicial = new MathGame(100);
     magicial.hit(4)
     expect(magicial.attack).toBe(70);
   });
   test('атака мага 5 клетки 60%', () => {
-    const magicial = new Math(100);
+    const magicial = new MathGame(100);
     magicial.hit(5)
     expect(magicial.attack).toBe(60);
   });
   test('атака мага 0 клетки невозможна', () => {
-    const magicial = new Math(100);
+    const magicial = new MathGame(100);
     magicial.hit(0)
     expect(magicial.attack).toBe(undefined);
   });
